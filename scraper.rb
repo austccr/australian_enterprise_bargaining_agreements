a9ca3385a2b8167c7fb034f93d596796b1aefc6b
Luke Bacon <luke@equivalentideas.com>
2019-06-10 13:39:03 +0200

    Extract method

--- scraper.rb
+++ scraper.rb
@@ -24,14 +24,54 @@ def new_agreement
     trading_name: nil,
     abn: nil,
     state: nil,
-    member_name: nil,
     matter_title: nil,
-    matter_number: nil,
     prefixed_print_id: nil,
-    print: nil
+    print: nil,
+    scraped_at: Time.now.utc.to_s
   }
 end
-#
+
+def assign_detail_fields(detail_fields, agreement)
+  detail_fields.map {|f| f.children.map(&:text) }.each do |label, value|
+    case label
+    when "Agreement Type description: "
+      agreement[:type] = value
+    when "Agreement Code description: "
+      agreement[:code_description] = value
+    when "Industry: "
+      agreement[:industry] = value
+    when "Old Pub Code: "
+      agreement[:old_pub_code] = value
+    when "Original Dispute Number: "
+      agreement[:original_dispute_number] = value
+    when "Publication ID: "
+      agreement[:publication_id] = value
+    when "Version: "
+      agreement[:version] = value
+    when "State: "
+      agreement[:state] = value
+    when "ABN: "
+      agreement[:abn] = value
+    when "Legal Name: "
+      agreement[:legal_name] = value
+    when "Trading Name: "
+      agreement[:trading_name] = value
+    when "Created Date: "
+      agreement[:created_at] = value
+    when "Last Modified Date: "
+      agreement[:modified_at] = value
+    when "Matter Title: "
+      agreement[:matter_title] = value
+    when "Prefixed Print ID: "
+      agreement[:prefixed_print_id] = value
+    when "Print: "
+      agreement[:print] = value
+    end
+  end
+
+  agreement
+end
+
 # INDEX_URL = "https://www.fwc.gov.au/search/document/agreement?items_per_page=100"
 
 agent = Mechanize.new
@@ -49,48 +89,20 @@ agreement = new_agreement
 table = agreement_page.at('#block-system-main')
 
 agreement[:reference_number] = agreement_page.uri.path.split('/').last
+
+puts "Collecting EBA #{agreement_page[:reference_number]}"
+
 agreement[:attached_document_url] = table.at('.field-name-field-fwc-doc-pdf-file a')[:href]
 agreement[:title] = agreement_page.at('#page-title').text
 
-detail_fields = table.at('.entity-fwc-agreement').search('.field')
-detail_fields.map {|f| f.children.map(&:text) }.each do |label, value|
-  case label
-  when "Agreement Type description: "
-    agreement[:type] = value
-  when "Agreement Code description: "
-    agreement[:code_description] = value
-  when "Industry: "
-    agreement[:industry] = value
-  when "Old Pub Code: "
-    agreement[:old_pub_code] = value
-  when "Original Dispute Number: "
-    agreement[:original_dispute_number] = value
-  when "Publication ID: "
-    agreement[:publication_id] = value
-  when "Version: "
-    agreement[:version] = value
-  when "State: "
-    agreement[:state] = value
-  when "ABN: "
-    agreement[:abn] = value
-  when "Legal Name: "
-    agreement[:legal_name] = value
-  when "Trading Name: "
-    agreement[:trading_name] = value
-  when "Created Date: "
-    agreement[:created_at] = value
-  when "Last Modified Date: "
-    agreement[:modified_at] = value
-  when "Matter Title: "
-    agreement[:matter_title] = value
-  when "Prefixed Print ID: "
-    agreement[:prefixed_print_id] = value
-  when "Print: "
-    agreement[:print] = value
-  end
-end
+agreement = assign_detail_fields(
+  table.at('.entity-fwc-agreement').search('.field'),
+  agreement
+)
 
 ScraperWiki.save_sqlite([:reference_number], agreement)
+puts "Saved EBA #{agreement_page[:reference_number]}"
+
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
