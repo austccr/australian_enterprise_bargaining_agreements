4818fea597ff190c34f5f9a4ec4feb303837205e
Luke Bacon <luke@equivalentideas.com>
2019-06-10 13:30:30 +0200

    Add initial scraper code for one agreement

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+
+## Questions
+
+* [  ] Is it useful to preserve 'N/A' values, or should they be skipped/left empty?
+* [  ] Where does 'Member name' come from?
--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,96 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
+
+def new_agreement
+  {
+    reference_number: nil,
+    attached_document_url: nil,
+    attached_document_syndicated_url: nil,
+    title: nil,
+    type: nil,
+    code_description: nil,
+    created_at: nil,
+    modified_at: nil,
+    details: nil,
+    publication_id: nil,
+    old_pub_code: nil,
+    original_dispute_number: nil,
+    version: nil,
+    industry: nil,
+    legal_name: nil,
+    trading_name: nil,
+    abn: nil,
+    state: nil,
+    member_name: nil,
+    matter_title: nil,
+    matter_number: nil,
+    prefixed_print_id: nil,
+    print: nil
+  }
+end
 #
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+# INDEX_URL = "https://www.fwc.gov.au/search/document/agreement?items_per_page=100"
+
+agent = Mechanize.new
+
+# page = agent.get(INDEX_URL)
+
+# agreement_links = page.links.select {|l| l.text.eql? 'More info' }
+
+# For each | test with first
+# agreement_page = agreement_links.first.click
+
+agreement_page  = agent.get('https://www.fwc.gov.au/document/agreement/AE502436')
+agreement = new_agreement
+
+table = agreement_page.at('#block-system-main')
+
+agreement[:reference_number] = agreement_page.uri.path.split('/').last
+agreement[:attached_document_url] = table.at('.field-name-field-fwc-doc-pdf-file a')[:href]
+agreement[:title] = agreement_page.at('#page-title').text
+
+detail_fields = table.at('.entity-fwc-agreement').search('.field')
+detail_fields.map {|f| f.children.map(&:text) }.each do |label, value|
+  case label
+  when "Agreement Type description: "
+    agreement[:type] = value
+  when "Agreement Code description: "
+    agreement[:code_description] = value
+  when "Industry: "
+    agreement[:industry] = value
+  when "Old Pub Code: "
+    agreement[:old_pub_code] = value
+  when "Original Dispute Number: "
+    agreement[:original_dispute_number] = value
+  when "Publication ID: "
+    agreement[:publication_id] = value
+  when "Version: "
+    agreement[:version] = value
+  when "State: "
+    agreement[:state] = value
+  when "ABN: "
+    agreement[:abn] = value
+  when "Legal Name: "
+    agreement[:legal_name] = value
+  when "Trading Name: "
+    agreement[:trading_name] = value
+  when "Created Date: "
+    agreement[:created_at] = value
+  when "Last Modified Date: "
+    agreement[:modified_at] = value
+  when "Matter Title: "
+    agreement[:matter_title] = value
+  when "Prefixed Print ID: "
+    agreement[:prefixed_print_id] = value
+  when "Print: "
+    agreement[:print] = value
+  end
+end
+
+ScraperWiki.save_sqlite([:reference_number], agreement)
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
